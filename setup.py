f642e8300ea031b9c8803f60dee401c9900ca891
oscarcui <cuizi7@163.com>
2019-12-06 16:11:39 +0800

    release 0.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rqams_client",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     python_requires=">=3",
     install_requires=[
